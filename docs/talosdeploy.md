48322e30f83268d45025f07e753d25850cf6d0d5
Engiqueers Concordia IT <321612729+it-engiqueers@users.noreply.github.com>
2026-05-20 13:05:15 -0400

    Auto deploy script

--- docs/talosdeploy.md
+++ docs/talosdeploy.md
@@ -3,12 +3,14 @@
 ## apply-config
 You need to add --insecure for the initial setup
 
-[//]: # (// TODO create script to run apply with decrypted values)
+Use script/deploy.sh to deploy Talos configuration to a node
 
 ### ENA
-#### Talos config
+#### Image configuration
 https://factory.talos.dev/?arch=amd64&board=undefined&bootloader=auto&cmdline=-console+console%3Dtty1+console%3DttyS0&cmdline-set=true&extensions=-&platform=metal&secureboot=undefined&target=metal&version=1.13.2
+
 baremetal
+
 ```
 customization:
     extraKernelArgs:
@@ -17,15 +19,9 @@ customization:
         - console=ttyS0
 ```
 
-```bash
-patches=(patches/ena/*.yaml patches/worker/*.yaml patches/all/*.yaml)
-talosctl apply-config --insecure --nodes ena.engiqueersconcordia.ca --file worker.dec.yaml ${patches[*]/#/--config-patch }
-```
 
 ### Jenny
-#### Talos config
+#### Image configuration
 https://factory.talos.dev/?arch=amd64&platform=metal&schematic-id=9ed5fecdacb36b5c5427b87d409f1065cfb2df69b0f71c58b868d9d466d8dab3&secureboot=true&target=metal&version=1.13.2
+
 baremetal-secureboot
-
-
-## Backup LUKS volume key
---
+++ script/deploy.sh
@@ -0,0 +1,66 @@
+#!/usr/bin/env bash
+
+# This script deploys talos config
+
+set -euo pipefail
+
+cd "$(dirname "$0")/.."
+source script/.filelist
+
+usage() {
+  echo "Usage: $0 <ena | jenny> [extra args...]"
+  exit 1
+}
+
+cleanup() {
+  if [[ -n "$decrypted_dir" ]]; then
+    rm -rf "$decrypted_dir"
+  fi
+  rm -f talos/controlplane.yaml talos/worker.yaml talos/talosconfig
+}
+trap 'cleanup' EXIT
+
+if [ "$#" -eq 0 ]; then
+  usage
+fi
+
+host=$1
+extra_args=("${@:2}")
+
+# Create folder to hold decrypted patches
+decrypted_dir=$(readlink -f "talos/patches_decrypted")
+rm -rf "$decrypted_dir"
+cp -r talos/patches "$decrypted_dir"
+for encrypted_file in "${SOPS_FILES[@]}"; do
+  # Only consider files in patches
+  if [[ "$encrypted_file" != talos/patches/* ]]; then
+    continue
+  fi
+
+  encrypted_file="${encrypted_file/\/patches\//\/patches_decrypted\/}"
+  echo "Decrypting $(basename "$encrypted_file")..."
+  sops decrypt -i "$encrypted_file"
+done
+
+# shellcheck disable=SC2016
+(
+cd talos
+sops exec-file secrets.yaml "sh -c 'talosctl gen config engiqueers https://cluster.engiqueersconcordia.ca:6443 --force --with-secrets \"\$1\" --kubernetes-version v1.36.0 --talos-version v1.13.2 --additional-sans cluster.engiqueersconcordia.ca' _ \"{}\""
+)
+
+case $host in
+ena)
+  patches=("$decrypted_dir"/ena/*.yaml "$decrypted_dir"/controlplane/*.yaml "$decrypted_dir"/all/*.yaml)
+  # shellcheck disable=SC2068
+  talosctl apply-config --nodes ena.engiqueersconcordia.ca --file talos/controlplane.yaml ${patches[@]/#/--config-patch } "${extra_args[@]}"
+  ;;
+jenny)
+  patches=("$decrypted_dir"/jenny/*.yaml "$decrypted_dir"/controlplane/*.yaml "$decrypted_dir"/all/*.yaml)
+  # shellcheck disable=SC2068
+  talosctl apply-config --nodes jenny.engiqueersconcordia.ca --file talos/controlplane.yaml ${patches[@]/#/--config-patch } "${extra_args[@]}"
+  ;;
+*)
+  echo "Unknown host $1"
+  usage
+  ;;
+esac
